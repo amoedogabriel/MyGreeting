3312bcc8604a57cae9175bb1cf6cd2a442a01941
Gabriel Amoêdo <75450592+amoedogabriel@users.noreply.github.com>
2023-08-21 16:49:32 -0400

    Criando lista de mensagem

--- src/main/java/example/myGreeting/Greeting.java
+++ src/main/java/example/myGreeting/Greeting.java
@@ -1,8 +1,36 @@
 package example.myGreeting;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Greeting {
 
-    public Greeting(long incrementAndGet, String format) {
+    private final long id;
+    private final String content;
+    private final List<String> greetings;
+
+
+
+    public Greeting(long id, String content){
+        this.id = id;
+        this.content = content;
+        this.greetings = new ArrayList<>();
     }
+
+    public long getId(){
+        return id;
+    }   
+
+    public String getContent(){
+        return content;
+    }   
+
+    public List<String> getGreetings(){
+        return greetings;
+    }
+
+
+
+
 
 }
--- src/main/java/example/myGreeting/counter.java
+++
@@ -1,5 +0,0 @@
-package example.myGreeting;
-
-public class counter {
-
-}
